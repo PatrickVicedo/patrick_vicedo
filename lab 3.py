92ac70344765799080fcbc0cc5956b1de17e2d2b
PatrickVicedo <patrickvicedo32@gmail.com>
2024-09-26 09:30:01 +0800

    lab 3.py

--- lab 3.py
+++ lab 3.py
@@ -3,15 +3,15 @@ monthly_salary = float(input("Monthly Salary:"))
 if monthly_salary >= 30000:
     print("Eligible")
     loan_amount  = float(input("Loan Amount:"))
-    loan_check = monthly_salary * 10
-    if loan_amount <= loan_check:
+    loan = monthly_salary * 10
+    if loan_amount <= loan:
         print("Eligible")
         months = int(input("Months:"))
         interest = loan_amount * 0.10
         loan_amount = interest + loan_amount
         payable = loan_amount/months
-        print("Total Pay:", loan_amount)
-        print("Monthly Pay:",payable) 
+        print("Total Payment:", loan_amount)
+        print("Monthly Payment:",payable) 
     else:
         print("Not Eligible: Loan Request Too High")
 else:
